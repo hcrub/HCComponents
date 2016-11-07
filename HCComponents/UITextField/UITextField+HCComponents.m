8a733bae2019156fffa646c72b68c2384013c371
Neil Burchfield <neil@nativ.com>
2016-11-06 22:14:35 -0600

    testing placeholder mutation

--- HCComponents/UITextField/UITextField+HCComponents.m
+++ HCComponents/UITextField/UITextField+HCComponents.m
@@ -12,6 +12,21 @@
 
 - (void)hc_setAppearanceFontName:(NSString *)fontName UI_APPEARANCE_SELECTOR {
     self.font = [UIFont fontWithName:fontName size:self.font.pointSize];
+    
+    if (!self.attributedPlaceholder) return;
+    
+    NSMutableAttributedString *mAttributedPlaceholder = self.attributedPlaceholder.mutableCopy;
+    CGFloat placeholderFontSize = self.font.pointSize;
+    
+    NSRange *range;
+    UIFont *placeholderFont = [self.attributedPlaceholder attribute:NSFontAttributeName atIndex:0 effectiveRange:range];
+    if (placeholderFont) {
+        placeholderFontSize = placeholderFont.pointSize;
+    }
+    
+    [mAttributedPlaceholder setAttributes:@{NSFontAttributeName:[UIFont fontWithName:fontName size:placeholderFontSize]} range:*range];
+    
+    self.attributedPlaceholder = [mAttributedPlaceholder copy];
 }
 
 - (void)hc_setAppearanceFont:(UIFont *)font UI_APPEARANCE_SELECTOR {
