37e287e3554a01863b769109f8c6705879fba85b
Neil Burchfield <neil@nativ.com>
2016-11-06 23:08:42 -0600

    UITextField updates

--- HCComponents/UITextField/UITextField+HCComponents.m
+++ HCComponents/UITextField/UITextField+HCComponents.m
@@ -11,26 +11,43 @@
 @implementation UITextField (HCComponents)
 
 - (void)hc_setAppearanceFontName:(NSString *)fontName UI_APPEARANCE_SELECTOR {
-    self.font = [UIFont fontWithName:fontName size:self.font.pointSize];
-    
-    if (!self.attributedPlaceholder) return;
-    
-    NSMutableAttributedString *mAttributedPlaceholder = self.attributedPlaceholder.mutableCopy;
-    CGFloat placeholderFontSize = self.font.pointSize;
-    
-    NSRange *range;
-    UIFont *placeholderFont = [self.attributedPlaceholder attribute:NSFontAttributeName atIndex:0 effectiveRange:range];
-    if (placeholderFont) {
-        placeholderFontSize = placeholderFont.pointSize;
-    }
-    
-    [mAttributedPlaceholder setAttributes:@{NSFontAttributeName:[UIFont fontWithName:fontName size:placeholderFontSize]} range:*range];
-    
-    self.attributedPlaceholder = [mAttributedPlaceholder copy];
+    [self hc_setAppearanceFont:[UIFont fontWithName:fontName size:self.font.pointSize]];
 }
 
 - (void)hc_setAppearanceFont:(UIFont *)font UI_APPEARANCE_SELECTOR {
     self.font = font;
+    
+    CGFloat placeholderFontSize = roundf(font.pointSize * 0.7f);
+    
+    if (self.attributedPlaceholder) {
+        NSMutableAttributedString *mAttributedPlaceholder = self.attributedPlaceholder.mutableCopy;
+        
+        [mAttributedPlaceholder beginEditing];
+        
+        NSRange range;
+        UIFont *existingPlaceholderFont = [self.attributedPlaceholder attribute:NSFontAttributeName atIndex:0 effectiveRange:&range];
+        if (existingPlaceholderFont) {
+            placeholderFontSize = existingPlaceholderFont.pointSize;
+        }
+        
+        [mAttributedPlaceholder addAttribute:NSFontAttributeName
+                                       value:[UIFont fontWithName:font.fontName size:placeholderFontSize]
+                                       range:range];
+        
+        [mAttributedPlaceholder endEditing];
+        
+        self.attributedPlaceholder = [mAttributedPlaceholder copy];
+    }
+    
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wundeclared-selector"
+    
+    if ([self respondsToSelector:@selector(setFloatingLabelFont:)]) {
+        [self performSelector:@selector(setFloatingLabelFont:) withObject:[UIFont fontWithName:font.fontName size:placeholderFontSize]];
+    }
+    
+#pragma clang diagnostic pop
+    
 }
 
 @end
