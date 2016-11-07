1c81f0b27837917af5acfed0cbc9da0ec224c2a2
Neil Burchfield <neil@nativ.com>
2016-11-06 20:48:25 -0600

    import header

---
+++ HCComponents/HCComponents.h
@@ -0,0 +1,23 @@
+//
+//  HCComponents.h
+//  HCComponents
+//
+//  Created by Neil Burchfield on 11/6/16.
+//  Copyright © 2016 Neil Burchfield. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HCComponents.
+FOUNDATION_EXPORT double HCComponentsVersionNumber;
+
+//! Project version string for HCComponents.
+FOUNDATION_EXPORT const unsigned char HCComponentsVersionString[];
+
+#import "NSBundle.h"
+#import "UIButton.h"
+#import "UIColor.h"
+#import "UIFont.h"
+#import "UIImage.h"
+#import "UILabel.h"
+#import "UITextField.h"
